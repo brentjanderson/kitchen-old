b37cf8122967531d12a76913151376d9313d1a84
Brent Anderson <brentj84062@gmail.com>
2012-05-13 15:03:11 -0600

    Working everything we need out.

--- protected/config/test.php
+++ protected/config/test.php
@@ -1,17 +1,15 @@
 <?php
 
 return CMap::mergeArray(
-	require(dirname(__FILE__).'/main.php'),
-	array(
-		'components'=>array(
-			'fixture'=>array(
-				'class'=>'system.test.CDbFixtureManager',
-			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
-		),
-	)
+                require(dirname(__FILE__) . '/main.php'), array(
+            'components' => array(
+                'db' => array(
+                    'connectionString' => 'mysql:host=localhost;dbname=aclan_kitchen',
+                    'emulatePrepare' => true,
+                    'username' => 'root',
+                    'password' => 'root',
+                    'charset' => 'utf8',
+                ),
+            ),
+                )
 );
--- www/index.php
+++ www/index.php
@@ -2,7 +2,14 @@
 
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii-1.1.10.r3566/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $config = 'dev';
+} else {
+    $config = 'main';
+}
+
+$config=dirname(__FILE__).'/../protected/config/'.$config.'.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
